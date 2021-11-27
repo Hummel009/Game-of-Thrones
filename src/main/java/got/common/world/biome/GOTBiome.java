66c19dfb7e9fb6cbf0223966ec2c8e990b317521
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2021-11-27 14:54:22 +0300

    remove biomes

--- src/main/java/got/common/world/biome/GOTBiome.java
+++ src/main/java/got/common/world/biome/GOTBiome.java
@@ -155,8 +155,6 @@ public abstract class GOTBiome extends BiomeGenBase {
 	public static GOTBiome shadowLand;
 	public static GOTBiome shadowMountains;
 	public static GOTBiome shadowTown;
-	public static GOTBiome farEast;
-	public static GOTBiome farEastForest;
 	public static GOTBiome skagos;
 	public static GOTBiome skirlingPass;
 	public static GOTBiome sothoryosBushland;
@@ -1131,7 +1129,7 @@ public abstract class GOTBiome extends BiomeGenBase {
 		shadowLand = new GOTBiomeShadowLand(id++, true).setMinMaxHeight(0.1f, 0.15f).setTemperatureRainfall(1.0f, 0.2f).setColor(0x8E8854).setBiomeName("shadowLand");
 		shadowMountains = new GOTBiomeShadowMountains(id++, true).setMinMaxHeight(2.0f, 2.0f).setTemperatureRainfall(1.0f, 0.2f).setColor(0x635E3B).setBiomeName("shadowMountains");
 		shadowTown = new GOTBiomeShadowTown(id++, true).setMinMaxHeight(0.1f, 0.0f).setTemperatureRainfall(1.0f, 0.2f).setColor(0x8C8340).setBiomeName("shadowTown");
-		farEast = new GOTBiomeFarEast(id++, true).setIsNeverWinter().setMinMaxHeight(0.1f, 0.05f).setColor(0xA39E60).setBiomeName("farEast");
+		id++;
 		skagos = new GOTBiomeSkagos(id++, true).setIsLongWinter().setMinMaxHeight(1.0f, 1.0f).setColor(0x94AF67).setBiomeName("skagos");
 		skirlingPass = new GOTBiomeSkirlingPass(id++, true).setIsAlwaysWinter().setMinMaxHeight(0.1f, 0.15f).setColor(0xC7E3EA).setBiomeName("skirlingPass");
 		sothoryosBushland = new GOTBiomeSothoryosBushland(id++, true).setIsNeverWinter().setMinMaxHeight(0.1f, 0.15f).setColor(0x998F3D).setBiomeName("sothoryosBushland");
@@ -1186,7 +1184,6 @@ public abstract class GOTBiome extends BiomeGenBase {
 		giftNewFlat = new GOTBiomeGiftNewFlat(id++, true).setIsLongWinter().setMinMaxHeight(0.1f, 0.0f).setColor(0x9CC453).setBiomeName("giftNew");
 		giftOldFlat = new GOTBiomeGiftOldFlat(id++, true).setIsAlwaysWinter().setMinMaxHeight(0.1f, 0.0f).setColor(0xA5C682).setBiomeName("giftOld");
 		ulos = new GOTBiomeUlos(id++, true).setIsNeverWinter().setMinMaxHeight(0.1f, 0.15f).setColor(0x2B4819).setBiomeName("ulos");
-		farEastForest = new GOTBiomeFarEastForest(id++, true).setIsNeverWinter().setMinMaxHeight(0.1f, 0.05f).setColor(0x6B8450).setBiomeName("farEastForest");
 	}
 
 	public static void updateWaterColor(int i, int j, int k) {
