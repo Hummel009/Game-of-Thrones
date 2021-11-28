c2304f4e75732720502d6642a8a50cbb6861d4e8
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2021-11-28 14:26:38 +0300

    Asshai stonks

--- src/main/java/got/common/world/biome/essos/GOTBiomeShadowMountains.java
+++ src/main/java/got/common/world/biome/essos/GOTBiomeShadowMountains.java
@@ -7,14 +7,20 @@ import got.client.sound.GOTBiomeMusic.MusicRegion;
 import got.common.database.*;
 import got.common.world.biome.GOTBiome;
 import got.common.world.biome.variant.GOTBiomeVariant;
+import got.common.world.feature.*;
 import got.common.world.map.GOTWaypoint;
 import got.common.world.map.GOTWaypoint.Region;
 import got.common.world.structure.other.GOTStructureRuinsBig;
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.NoiseGeneratorPerlin;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 
 public class GOTBiomeShadowMountains extends GOTBiome {
+	public static NoiseGeneratorPerlin noiseDirt = new NoiseGeneratorPerlin(new Random(389502092662L), 1);
+	public static NoiseGeneratorPerlin noiseGravel = new NoiseGeneratorPerlin(new Random(1379468206L), 1);
+
 	public GOTBiomeShadowMountains(int i, boolean major) {
 		super(i, major);
 		topBlock = GOTRegistry.rock;
@@ -29,31 +35,93 @@ public class GOTBiomeShadowMountains extends GOTBiome {
 		decorator.addOre(new WorldGenMinable(GOTRegistry.oreGlowstone, 4), 8.0f, 0, 48);
 		decorator.addOre(new WorldGenMinable(GOTRegistry.oreCobalt, 5), 5.0f, 0, 32);
 		decorator.clearTrees();
-		decorator.treesPerChunk = -1;
+		decorator.flowersPerChunk = 0;
+		decorator.grassPerChunk = 1;
+		decorator.sandPerChunk = 0;
+		decorator.clayPerChunk = 0;
+		decorator.dryReedChance = 1.0f;
+		enableRocky = false;
+		decorator.addTree(GOTTreeType.CHARRED, 1000);
 		biomeColors.setSky(0);
 		biomeColors.setClouds(0);
 		biomeColors.setFog(0);
 		biomeColors.setWater(0);
-		enableRocky = true;
 		GOTStructureRuinsBig colossal = new GOTStructureRuinsBig(this, 0.0f);
 		colossal.affix(GOTWaypoint.Stygai);
 		decorator.affix(colossal);
 	}
-	
+
+	@Override
+	public void decorate(World world, Random random, int i, int k) {
+		int j1;
+		int i1;
+		int k1;
+		super.decorate(world, random, i, k);
+		int l;
+		int k12;
+		int i12;
+		int j12;
+		if (random.nextInt(60) == 0) {
+			for (l = 0; l < 8; ++l) {
+				i12 = i + random.nextInt(16) + 8;
+				k12 = k + random.nextInt(16) + 8;
+				j12 = world.getHeightValue(i12, k12);
+				decorator.genTree(world, random, i12, j12, k12);
+			}
+		}
+		if (decorator.grassPerChunk > 0) {
+			if (random.nextInt(20) == 0) {
+				for (l = 0; l < 6; ++l) {
+					i12 = i + random.nextInt(6) + 8;
+					if (!world.isAirBlock(i12, j12 = world.getHeightValue(i12, k12 = k + random.nextInt(6) + 8), k12) || !GOTRegistry.asshaiThorn.canBlockStay(world, i12, j12, k12)) {
+						continue;
+					}
+					world.setBlock(i12, j12, k12, GOTRegistry.asshaiThorn, 0, 2);
+				}
+			}
+			if (random.nextInt(20) == 0 && world.isAirBlock(i1 = i + random.nextInt(16) + 8, j1 = world.getHeightValue(i1, k1 = k + random.nextInt(16) + 8), k1) && GOTRegistry.asshaiMoss.canBlockStay(world, i1, j1, k1)) {
+				new GOTWorldGenAsshaiMoss().generate(world, random, i1, j1, k1);
+			}
+		}
+	}
+
+	@Override
+	public void generateBiomeTerrain(World world, Random random, Block[] blocks, byte[] meta, int i, int k, double stoneNoise, int height, GOTBiomeVariant variant) {
+		Block topBlock_pre = topBlock;
+		int topBlockMeta_pre = topBlockMeta;
+		Block fillerBlock_pre = fillerBlock;
+		int fillerBlockMeta_pre = fillerBlockMeta;
+		double d1 = noiseDirt.func_151601_a(i * 0.08, k * 0.08);
+		double d2 = noiseDirt.func_151601_a(i * 0.4, k * 0.4);
+		double d3 = noiseGravel.func_151601_a(i * 0.08, k * 0.08);
+		if (d3 + (noiseGravel.func_151601_a(i * 0.4, k * 0.4)) > 0.8) {
+			topBlock = GOTRegistry.basaltGravel;
+			topBlockMeta = 0;
+			fillerBlock = topBlock;
+			fillerBlockMeta = topBlockMeta;
+		} else if (d1 + d2 > 0.5) {
+			topBlock = GOTRegistry.asshaiDirt;
+			topBlockMeta = 0;
+			fillerBlock = topBlock;
+			fillerBlockMeta = topBlockMeta;
+		}
+		super.generateBiomeTerrain(world, random, blocks, meta, i, k, stoneNoise, height, variant);
+		topBlock = topBlock_pre;
+		topBlockMeta = topBlockMeta_pre;
+		fillerBlock = fillerBlock_pre;
+		fillerBlockMeta = fillerBlockMeta_pre;
+	}
+
 	@Override
 	public void generateMountainTerrain(World world, Random random, Block[] blocks, byte[] meta, int i, int k, int xzIndex, int ySize, int height, int rockDepth, GOTBiomeVariant variant) {
-		int stoneHeight = 110 - rockDepth;
-		int sandHeight = stoneHeight - 6;
-		for (int j = ySize - 1; j >= sandHeight; --j) {
+		for (int j = ySize - 1; j >= 0; --j) {
 			int index = xzIndex * ySize + j;
 			Block block = blocks[index];
-			if (block != topBlock && block != fillerBlock) {
+			if (block != Blocks.stone) {
 				continue;
 			}
-			if (j >= stoneHeight) {
-				blocks[index] = GOTRegistry.rock;
-				meta[index] = 0;
-			}
+			blocks[index] = GOTRegistry.rock;
+			meta[index] = 0;
 		}
 	}
 
@@ -70,6 +138,11 @@ public class GOTBiomeShadowMountains extends GOTBiome {
 	@Override
 	public Region getBiomeWaypoints() {
 		return Region.ASSHAI;
+	}
+
+	@Override
+	public GrassBlockAndMeta getRandomGrass(Random random) {
+		return new GrassBlockAndMeta(GOTRegistry.asshaiGrass, 0);
 	}
 
 	public static boolean isBasalt(World world, int i, int j, int k) {
---
+++ src/main/java/got/common/world/feature/GOTWorldGenAsshaiMoss.java
@@ -0,0 +1,43 @@
+package got.common.world.feature;
+
+import java.util.Random;
+
+import got.common.database.GOTRegistry;
+import got.common.world.biome.essos.GOTBiomeShadowMountains;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
+
+public class GOTWorldGenAsshaiMoss extends WorldGenerator {
+	public boolean generate(World world, Random random, int i, int j, int k) {
+		int numberOfMoss = 32 + random.nextInt(80);
+		float f = random.nextFloat() * 3.1415927f;
+		double d = (float) (i + 8) + MathHelper.sin((float) f) * (float) numberOfMoss / 8.0f;
+		double d1 = (float) (i + 8) - MathHelper.sin((float) f) * (float) numberOfMoss / 8.0f;
+		double d2 = (float) (k + 8) + MathHelper.cos((float) f) * (float) numberOfMoss / 8.0f;
+		double d3 = (float) (k + 8) - MathHelper.cos((float) f) * (float) numberOfMoss / 8.0f;
+		for (int l = 0; l <= numberOfMoss; ++l) {
+			double d5 = d + (d1 - d) * (double) l / (double) numberOfMoss;
+			double d6 = d2 + (d3 - d2) * (double) l / (double) numberOfMoss;
+			double d7 = random.nextDouble() * (double) numberOfMoss / 16.0;
+			double d8 = (double) (MathHelper.sin((float) ((float) l * 3.1415927f / (float) numberOfMoss)) + 1.0f) * d7 + 1.0;
+			int i1 = MathHelper.floor_double((double) (d5 - d8 / 2.0));
+			int k1 = MathHelper.floor_double((double) (d6 - d8 / 2.0));
+			int i2 = MathHelper.floor_double((double) (d5 + d8 / 2.0));
+			int k2 = MathHelper.floor_double((double) (d6 + d8 / 2.0));
+			for (int i3 = i1; i3 <= i2; ++i3) {
+				double d9 = ((double) i3 + 0.5 - d5) / (d8 / 2.0);
+				if (!(d9 * d9 < 1.0))
+					continue;
+				for (int k3 = k1; k3 <= k2; ++k3) {
+					double d10;
+					int j1 = world.getHeightValue(i3, k3);
+					if (j1 != j || !(d9 * d9 + (d10 = ((double) k3 + 0.5 - d6) / (d8 / 2.0)) * d10 < 1.0) || !GOTBiomeShadowMountains.isBasalt(world, i3, j1 - 1, k3) || world.getBlockMetadata(i3, j1 - 1, k3) != 0 || !world.isAirBlock(i3, j1, k3))
+						continue;
+					world.setBlock(i3, j1, k3, GOTRegistry.asshaiMoss, 0, 2);
+				}
+			}
+		}
+		return true;
+	}
+}
