4c389ee854a071df07f65f9ee112d5129cffca35
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2021-12-01 06:12:10 +0300

    Update GOTInfo.java

--- src/main/java/got/GOTInfo.java
+++ src/main/java/got/GOTInfo.java
@@ -3,7 +3,7 @@ package got;
 import net.minecraft.util.StatCollector;
 
 public class GOTInfo {
-	public static String[] description = { "\u00A76" + StatCollector.translateToLocal("got.gui.authors") + " Hummel009, TIGASA, Monitor, Iliamakar, DAndMaster, JaqenHghar01, beautifulrobloxgirl01, Sword of the Morning " + StatCollector.translateToLocal("got.gui.authors.others") };
+	public static String[] description = { "\u00A76" + StatCollector.translateToLocal("got.gui.authors") + " Hummel009, TIGASA, DAndMaster, JaqenHghar01, RoobimWu, Xiang SiMa, beautifulrobloxgirl01, Sword of the Morning" + StatCollector.translateToLocal("got.gui.authors.others") };
 
 	public static String concatenateDescription(int startIndex) {
 		StringBuilder s = new StringBuilder();
