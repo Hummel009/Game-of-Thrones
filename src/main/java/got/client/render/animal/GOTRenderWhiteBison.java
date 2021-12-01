f0a931b524d08c2f60506a47645ece9bb2a40c1f
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2021-12-01 19:57:27 +0300

    Update GOTRenderWhiteBison.java

--- src/main/java/got/client/render/animal/GOTRenderWhiteBison.java
+++ src/main/java/got/client/render/animal/GOTRenderWhiteBison.java
@@ -1,10 +1,8 @@
 package got.client.render.animal;
 
-import org.lwjgl.opengl.GL11;
-
 import got.client.render.other.GOTRandomSkins;
 import got.common.entity.animal.GOTEntityWhiteBison;
-import net.minecraft.entity.*;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 
 public class GOTRenderWhiteBison extends GOTRenderBison {
@@ -18,16 +16,5 @@ public class GOTRenderWhiteBison extends GOTRenderBison {
 	public ResourceLocation getEntityTexture(Entity entity) {
 		GOTEntityWhiteBison bison = (GOTEntityWhiteBison) entity;
 		return wbisonSkins.getRandomSkin(bison);
-	}
-
-	@Override
-	public void preRenderCallback(EntityLivingBase entity, float f) {
-		super.preRenderCallback(entity, f);
-		GOTEntityWhiteBison animal = (GOTEntityWhiteBison) entity;
-		if (animal.isChild()) {
-			GL11.glScalef(0.55f, 0.55f, 0.55f);
-		} else {
-			GL11.glScalef(1.15f, 1.15f, 1.15f);
-		}
 	}
 }
