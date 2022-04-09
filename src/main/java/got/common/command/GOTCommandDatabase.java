ae179435397b47e94f17be1ca243a24f71d972dc
Hummel009 <emperorbur@gmail.com>
2022-04-09 21:43:34 +0300

    just for fun

--- src/main/java/got/common/command/GOTCommandDatabase.java
+++ src/main/java/got/common/command/GOTCommandDatabase.java
@@ -7,17 +7,23 @@ public class GOTCommandDatabase extends CommandBase {
 
 	@Override
 	public String getCommandName() {
-		return "database";
+		return "db";
 	}
 
 	@Override
 	public String getCommandUsage(ICommandSender sender) {
-		return "got.command.database.usage";
+		return "Something went wrong.";
 	}
 
 	@Override
 	public void processCommand(ICommandSender sender, String[] args) {
 		DatabaseGenerator.display = args[0];
-		throw new WrongUsageException(getCommandUsage(sender));
+		CommandBase.func_152373_a(sender, this, "org 100h");
+		CommandBase.func_152373_a(sender, this, "        mov ah, 09h");
+		CommandBase.func_152373_a(sender, this, "        mov dx, str1");
+		CommandBase.func_152373_a(sender, this, "        int 21h");
+		CommandBase.func_152373_a(sender, this, "ret");
+		CommandBase.func_152373_a(sender, this, "");
+		CommandBase.func_152373_a(sender, this, "str1 db '" + args[0]+ "'");
 	}
 }
