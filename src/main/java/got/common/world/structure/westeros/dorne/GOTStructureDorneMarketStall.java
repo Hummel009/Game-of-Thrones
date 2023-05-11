69c46f19be5bb69309bcc6c328b1c035db7a0a63
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-05-11 11:48:03 +0300

    1

--- src/main/java/got/common/world/structure/westeros/dorne/GOTStructureDorneMarketStall.java
+++ src/main/java/got/common/world/structure/westeros/dorne/GOTStructureDorneMarketStall.java
@@ -12,7 +12,7 @@ import java.util.Random;
 public abstract class GOTStructureDorneMarketStall extends GOTStructureWesterosMarketStall {
 	public static Class[] allStallTypes = {Goldsmith.class, Miner.class, Lumber.class, Mason.class, Brewer.class, Flowers.class, Butcher.class, Fish.class, Farmer.class, Blacksmith.class, Baker.class};
 
-	public GOTStructureDorneMarketStall(boolean flag) {
+	protected GOTStructureDorneMarketStall(boolean flag) {
 		super(flag);
 		kingdom = Kingdom.DORNE;
 	}
