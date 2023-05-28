e6f8f13b4a114f8d14ca9995d66c07326112f223
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-05-28 16:16:26 +0300

    контрастнее

--- src/main/java/got/common/world/biome/GOTBiome.java
+++ src/main/java/got/common/world/biome/GOTBiome.java
@@ -426,7 +426,7 @@ public abstract class GOTBiome extends BiomeGenBase {
 		reachForest = new GOTBiomeReachForest(181, true).setClimateType(GOTClimateType.SUMMER).setColor(0x6e8241).setMinMaxHeight(0.1f, 0.15f).setBiomeName("reachForest");
 		reachHills = new GOTBiomeReach(141, true).setClimateType(GOTClimateType.SUMMER).setColor(0x8F9B4D).setMinMaxHeight(0.1f, 1.0f).setBiomeName("reachHills");
 		reachTown = new GOTBiomeReachTown(104, true).setClimateType(GOTClimateType.SUMMER).setColor(0xC2D87B).setMinMaxHeight(0.1f, 0.15f).setBiomeName("reachTown");
-		riverlands = new GOTBiomeRiverlands(109, true).setClimateType(GOTClimateType.NORMAL).setColor(0x82B756).setMinMaxHeight(0.1f, 0.15f).setBiomeName("riverlands");
+		riverlands = new GOTBiomeRiverlands(109, true).setClimateType(GOTClimateType.NORMAL).setColor(0x87BC58).setMinMaxHeight(0.1f, 0.15f).setBiomeName("riverlands");
 		riverlandsForest = new GOTBiomeRiverlandsForest(111, true).setClimateType(GOTClimateType.NORMAL).setColor(0x53823D).setMinMaxHeight(0.1f, 1.0f).setBiomeName("riverlandsForest");
 		shrykesLand = new GOTBiomeShrykesLand(90, true).setClimateType(GOTClimateType.SUMMER).setColor(0xAAAE77).setMinMaxHeight(0.0f, 0.1f).setBiomeName("shrykesLand");
 		skagos = new GOTBiomeSkagos(116, true).setClimateType(GOTClimateType.COLD).setColor(0x97BF85).setMinMaxHeight(0.1f, 0.15f).setBiomeName("skagos");
