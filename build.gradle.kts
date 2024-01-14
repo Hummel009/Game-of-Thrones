c24f0b099d657c67f2a453c0f37104667636e4e6
Hummel009 <hummel.turbamentum@gmail.com>
2024-01-14 17:21:13 +0300

    retrofuturagradle 1.3.28

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 plugins {
-	id("com.gtnewhorizons.retrofuturagradle") version "1.3.24"
+	id("com.gtnewhorizons.retrofuturagradle") version "1.3.28"
 }
 
 group = "hummel"
