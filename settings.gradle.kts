49d7cf6f0762807e4a74ca9b31bc905a7838a679
Hummel009 <hummel.turbamentum@gmail.com>
2024-01-29 13:59:30 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-	repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+	repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
 	repositories {
 		mavenLocal()
 		mavenCentral()
