21258fc5ca6cc49935193699121dda79bf3f6f42
Hummel009 <hummel.turbamentum@gmail.com>
2024-02-24 11:31:47 +0300

    Agripas twice

--- src/main/java/got/GOTInfo.java
+++ src/main/java/got/GOTInfo.java
@@ -3,7 +3,7 @@ package got;
 import net.minecraft.util.StatCollector;
 
 public class GOTInfo {
-	public static String[] description = {"§b" + StatCollector.translateToLocal("got.gui.authors") + " Hummel009, Octobrine, Agripas, VVVIP2, TIGASA, 安北都护府大都护-至高无上的Xiang SiMa, 贾昆·赫加尔01, RoobimWu, Agripas, Ness, Axel Snow, Sword of the Morning, sashar2000r (Starvation), iliamakar, beautifulrobloxgirl01 " + StatCollector.translateToLocal("got.gui.authors.others")};
+	public static String[] description = {"§b" + StatCollector.translateToLocal("got.gui.authors") + " Hummel009, Octobrine, Agripas, VVVIP2, TIGASA, 安北都护府大都护-至高无上的Xiang SiMa, 贾昆·赫加尔01, RoobimWu, Ness, Axel Snow, Sword of the Morning, sashar2000r (Starvation), iliamakar, beautifulrobloxgirl01 " + StatCollector.translateToLocal("got.gui.authors.others")};
 
 	public static String concatenateDescription(int startIndex) {
 		StringBuilder sb = new StringBuilder();
