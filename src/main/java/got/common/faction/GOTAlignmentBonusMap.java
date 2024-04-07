7fc7fde50f13df9d4b932bad69ec65ba35e46929
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-07 19:09:50 +0300

    Update GOTAlignmentBonusMap.java

--- src/main/java/got/common/faction/GOTAlignmentBonusMap.java
+++ src/main/java/got/common/faction/GOTAlignmentBonusMap.java
@@ -1,13 +1,13 @@
 package got.common.faction;
 
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 
-public class GOTAlignmentBonusMap extends HashMap<GOTFaction, Float> {
+public class GOTAlignmentBonusMap implements Map<GOTFaction, Float> {
+	private final Map<GOTFaction, Float> bonusMap = new EnumMap<>(GOTFaction.class);
+
 	public Set<GOTFaction> getChangedFactions() {
 		EnumSet<GOTFaction> changed = EnumSet.noneOf(GOTFaction.class);
-		for (Entry<GOTFaction, Float> fac : entrySet()) {
+		for (Map.Entry<GOTFaction, Float> fac : entrySet()) {
 			float bonus = fac.getValue();
 			if (bonus == 0.0f) {
 				continue;
@@ -15,5 +15,65 @@ public class GOTAlignmentBonusMap extends HashMap<GOTFaction, Float> {
 			changed.add(fac.getKey());
 		}
 		return changed;
+	}
+
+	@Override
+	public int size() {
+		return bonusMap.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return bonusMap.isEmpty();
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return bonusMap.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return bonusMap.containsValue(value);
+	}
+
+	@Override
+	public Float get(Object key) {
+		return bonusMap.get(key);
+	}
+
+	@Override
+	public Float put(GOTFaction key, Float value) {
+		return bonusMap.put(key, value);
+	}
+
+	@Override
+	public Float remove(Object key) {
+		return bonusMap.remove(key);
+	}
+
+	@Override
+	public void putAll(Map<? extends GOTFaction, ? extends Float> m) {
+		bonusMap.putAll(m);
+	}
+
+	@Override
+	public void clear() {
+		bonusMap.clear();
+	}
+
+	@Override
+	public Set<GOTFaction> keySet() {
+		return bonusMap.keySet();
+	}
+
+	@Override
+	public Collection<Float> values() {
+		return bonusMap.values();
+	}
+
+	@Override
+	public Set<Entry<GOTFaction, Float>> entrySet() {
+		return bonusMap.entrySet();
 	}
 }
