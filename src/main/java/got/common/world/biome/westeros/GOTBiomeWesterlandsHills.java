3eaa026223e845dbd07390386f8597a12813bdb3
Hummel009 <hummel.turbamentum@gmail.com>
2024-05-09 12:10:04 +0300

    Update GOTBiomeWesterlandsHills.java

--- src/main/java/got/common/world/biome/westeros/GOTBiomeWesterlandsHills.java
+++ src/main/java/got/common/world/biome/westeros/GOTBiomeWesterlandsHills.java
@@ -1,20 +1,14 @@
 package got.common.world.biome.westeros;
 
-import got.common.database.GOTBlocks;
 import got.common.entity.animal.GOTEntityLion;
 import got.common.entity.animal.GOTEntityLioness;
 import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.gen.feature.WorldGenMinable;
 
 public class GOTBiomeWesterlandsHills extends GOTBiomeWesterlands {
 	public GOTBiomeWesterlandsHills(int i, boolean major) {
 		super(i, major);
 		decorator.setBiomeOreFactor(2.0f);
 		decorator.setBiomeGemFactor(2.0f);
-		decorator.addOre(new WorldGenMinable(GOTBlocks.oreGlowstone, 4), 8.0f, 0, 48);
-		decorator.addOre(new WorldGenMinable(GOTBlocks.oreCobalt, 5), 5.0f, 0, 32);
-		decorator.clearSettlements();
-		npcSpawnList.clear();
 		spawnableCreatureList.add(new BiomeGenBase.SpawnListEntry(GOTEntityLion.class, 10, 1, 1));
 		spawnableCreatureList.add(new BiomeGenBase.SpawnListEntry(GOTEntityLioness.class, 10, 1, 1));
 	}
