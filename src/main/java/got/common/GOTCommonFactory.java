9814395f121efe1408336a611021414eaf2bd2f3
Hummel009 <hummel.turbamentum@gmail.com>
2024-07-10 22:01:08 +0300

    Update GOTCommonFactory.java

--- src/main/java/got/common/GOTCommonFactory.java
+++ src/main/java/got/common/GOTCommonFactory.java
@@ -6,8 +6,6 @@ import cpw.mods.fml.common.eventhandler.EventBus;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import got.GOT;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.IResourceManager;
 import net.minecraftforge.common.MinecraftForge;
 
 @SuppressWarnings("unused")
@@ -21,8 +19,6 @@ public class GOTCommonFactory {
 	}
 
 	public static void preInit() {
-		Minecraft mc = Minecraft.getMinecraft();
-		IResourceManager resourceManager = mc.getResourceManager();
 		EventBus forgeBus = MinecraftForge.EVENT_BUS;
 		EventBus terrainBus = MinecraftForge.TERRAIN_GEN_BUS;
 		EventBus fmlBus = FMLCommonHandler.instance().bus();
